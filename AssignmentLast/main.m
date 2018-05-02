a8c091815493e4002fc61352f1184c4284f3e135
Asami Tanii <g033.9521@gmail.com>
2018-05-02 14:05:46 -0700

    Initial Commit

---
+++ AssignmentLast/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AssignmentLast
+//
+//  Created by 谷井朝美 on 2018-05-02.
+//  Copyright © 2018 Asami Tanii. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
